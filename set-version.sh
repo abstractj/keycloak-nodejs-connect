39e0dfd6b04ee7b30de5bedb71b246b4956f1480
Stefan Guilhen <sguilhen@redhat.com>
2020-09-11 11:36:32 -0300

    Set version to 9.0.6

--- set-version.sh
+++ set-version.sh
@@ -4,4 +4,10 @@ VERSION=$1
 NPM_VERSION=`echo $VERSION | sed 's/.Final//' | sed 's/.CR/-cr./' | sed 's/.Beta/-beta./' | sed 's/.Alpha/-alpha./'`
 
 sed -i 's/"version": .*/"version": "'$NPM_VERSION'",/' package.json
+
+sed -i \
+    -e 's/^VERSION=.*/VERSION='$NPM_VERSION'/' \
+    -e 's/NPM_VERSION=.*/NPM_VERSION='$NPM_VERSION'/' \
+    release-details
+
 mvn -f product/pom.xml versions:set -DnewVersion=$NPM_VERSION -DgenerateBackupPoms=false
